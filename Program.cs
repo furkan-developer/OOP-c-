b2c3e5d39e257b0c04f5420d165a3ab267aa6b1d
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-07-19 18:37:37 +0300

    Overloading in indexer

--- Program.cs
+++ Program.cs
@@ -1,17 +1,35 @@
-﻿Numbers numbers = new();
+﻿using System.Text;
+
+Numbers numbers = new();
 
 numbers[1] = "One";
-numbers[2] = "Two";
+numbers[2,true] = "İki";
 
-System.Console.WriteLine(numbers[1]);
-// One
+System.Console.WriteLine(numbers[2]);
+// İki
+
 class Numbers
 {
-    Dictionary<int,string> numbers = new();
+    Dictionary<int, string> numbers = new();
 
     public object this[int number]
     {
         get { return numbers[number]; }
-        set { numbers.Add(number,(string)value); }
+        set { numbers.Add(number, (string)value); }
+    }
+
+    public object this[int number, bool isTurkish]
+    {
+        get { return numbers[number]; }
+        set
+        {   
+            StringBuilder builder = new((string)value);
+            if(isTurkish)
+                builder.Append(" -tr");
+
+            string result = builder.ToString();
+
+            numbers.Add(number,result);
+        }
     }
 }

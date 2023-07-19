5cfc6dfe9d80aa3bcad2f99b0da393774503e1f1
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-07-19 18:29:22 +0300

    Indexer

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Numbers numbers = new();
+
+numbers[1] = "One";
+numbers[2] = "Two";
+
+System.Console.WriteLine(numbers[1]);
+// One
+class Numbers
+{
+    Dictionary<int,string> numbers = new();
+
+    public object this[int number]
+    {
+        get { return numbers[number]; }
+        set { numbers.Add(number,(string)value); }
+    }
+}

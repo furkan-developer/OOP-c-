0f63813fbc62982c716e64067073489453b6fb95
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-07-22 17:02:15 +0300

    Type casting

--- Program.cs
+++ Program.cs
@@ -1,34 +1,15 @@
 ﻿
+// fiziksel olarak ram de oluşturulan c tipinin nesneni
+A a = new C() { };
+// D d = (D)a; // Run-time hata verir
+D d = a as D; // d = null ataması olur.
 
-Person person1 = new Person(Name:"Furkan",LastName:"Aydın");
-Person person2 = person1 with {Name="Ferdi"};
+if (d is null)
+{
+    System.Console.WriteLine("d is null");
+}
 
-// Deconstruct support tuple
-(string name, string lastName) = person1;
-
-System.Console.WriteLine($"name variable: {name}");
-// output: name variable: Furkan
-
-System.Console.WriteLine($"person1 Name: {person1.Name} -- person2 Name: {person2.Name}");
-// output: person1 Name: Furkan -- person2 Name: Ferdi
-
-record Person(string Name, string LastName);
-
-// following code created by compiler
-// record Person
-// {
-//     public string Name { get; init; }
-//     public string LastName { get; init; }
-    
-//     public Person(string Name, string LastName)
-//     {
-//         this.Name = Name;
-//         this.LastName = LastName;
-//     }
-
-//     public void Deconstruct(out string Name, out string LastName)
-//     {
-//         Name = this.Name;
-//         LastName = this.LastName;
-//     }
-// }
+class A { }
+class B : A { }
+class C : B { }
+class D : C { }
